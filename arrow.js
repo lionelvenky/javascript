1739b2f95d44e609ad032498650620744468df32
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-11-01 19:48:10 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,21 @@
+//function
+//let x = function(x, y) {
+ //   return x * y;
+ //}
+
+
+
+
+ // using arrow function
+let x = (x, y) => x * y;
+
+
+class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const person1 = new Person('venky');
+  
+  console.log(person1.name); 
