0c83b65194151c749f090648cf0e4edbfce58b0d
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-11-01 19:55:24 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,17 @@
+
+const n = 5;
+
+for (let i = 1; i <= n; i++) {
+    console.log(`my name is venky.`);
+}
+
+
+
+let sum = 0;
+const a = 100
+
+for (let i = 1; i <= n; i++) {
+    sum += i;  
+}
+
+console.log('sum:', sum);
