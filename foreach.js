a8aef9e92e98e5f05226949fcd2469d9abb1c58f
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-11-03 11:42:30 +0530

    Add files via upload

---
+++ foreach.js
@@ -0,0 +1,8 @@
+let scroes = [10,  20,  30 , 40 , 50];
+
+let players = ["sonu", "cherry", "harsha", "tharun"]
+
+players.forEach(function(item, index) {
+
+    console.log(index + 1 +"." +item  )
+})
