6dd8eb62dd40de9afe3c37683c8419edbf5a52be
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-11-04 11:31:14 +0530

    Add files via upload

---
+++ stringMethod.js
@@ -0,0 +1,26 @@
+//split 
+
+let num = "10 20 30 40"
+
+console.log (num.split(" "))
+
+//trim
+
+var player = "    messi   "
+
+console.log(player.trim())
+
+//charAt
+let student = "charan"
+console.log(student.charAt(4))
+console.log(student.charAt(0))
+
+//substring
+
+let me = "venky"
+console.log(me.substring(4))
+
+
+//replace
+console.log(me.replace("messi"))
+
