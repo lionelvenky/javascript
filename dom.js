36b122bb11f73a8bb2fb8059066ebb3da37a9676
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-11-07 21:30:19 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,15 @@
+//getElementsById
+
+//const a = document.getElementById("company")
+
+//a.innerText = "sales force"
+
+//a.innerHTML = "<h1>nwe title</h1>"
+
+//a.textContent = "new sales company";
+
+var myRevenue =document.getElementsByClassName("revenue")
+
+myRevenue.textcontent = "growth"
+
+console.log(myRevenue)
