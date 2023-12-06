beb720ad756e7807f6f2a46fd5a1fda017dbf421
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-12-06 10:53:28 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,29 @@
+
+let age = 25;           
+let name = "John";      
+let isAdult = true;     
+let person = {          
+    name: "John",
+    age: 25
+};
+
+
+
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+greet("venky");
+
+
+
+let hour = 12;
+
+if (hour < 12) {
+    console.log("Good morning!");
+} else if (hour < 18) {
+    console.log("Good afternoon!");
+} else {
+    console.log("Good evening!");
+}
+
