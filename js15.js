7341606c7b45ea193e088d78f97265c2765eba6a
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2024-01-01 20:26:44 +0530

    Add files via upload

---
+++ js15.js
@@ -0,0 +1,15 @@
+//indexof()
+
+
+let student = ["venky ","sonu","cherry","harsha" ];
+
+var newstudent = student.indexOf("tharun")
+
+if (newstudent=== -1) {
+    student.push("tharun")
+}
+
+//console.log (student.indexOf("charan"))
+console.log(student)
+
+
